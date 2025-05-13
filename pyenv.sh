410cd6dbe25c7fb390b0e3ce2846d22c2578c276
Wellington Sarmento <wwagner@virtual.ufc.br>
2025-05-13 13:58:23 -0300

    Uso de caching para grafisos e mapas, correcao do mapa interativo, melhoria na interface do mapa contextual

---
+++ pyenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script is used to run the Streamlit app with the specified configuration.
+
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+    source ./.venv/bin/activate
+else
+    echo "Activating virtual environment..."
+    source ./venv/bin/activate
+fi
+
