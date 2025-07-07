8a81770c8e6c95aaa13bd3968d55a28b52c4c54f
Andravi <andravi75@gmail.com>
2025-07-07 19:54:20 -0300

    todo.py add

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
 # app.py
-
-# TODO URGENTE: Fazer a carga uma unica vez e ficar usando os DF/GDFs carregados
-# DONE Refatorar este código colocando as funções de tratamento de dados e rederizacao nos modulos
-# DONE Padronizar todoas as legendas 
-# DONE Inserir nos mapas os mini-maps 
-# TODO Inserir a legenda e a cor de "<100 imoveis" nos mapas contextuais e Gini
-# TODO Inserir o "?" (Seção de "O que esse mapa é") Em todos as secoes com explicacoes sobre o que e apresentado
-# DONE Unificar os data_loaders e refatorar 
-# TODO Padronizar todas as tabelas para o estilo da tabela presente no Mapa de predominancia
-
-
 import streamlit as st
 from streamlit_folium import st_folium
 import folium
