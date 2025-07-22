d6ea9f72d839ba4c743cc77265d6ce23023ac084
Andravi <andravi75@gmail.com>
2025-07-22 17:07:42 -0300

    logos update

--- app.py
+++ app.py
@@ -168,19 +168,20 @@ def sobre():
     st.markdown("https://github.com/Projeto-Cientista-Chefe-Terra")
 
     st.header("Apoio")
+    st.image("./assets/Logos.png", use_container_width=True)
     col1, col2, col3, col4 = st.columns(4, vertical_alignment='center', )
 
-    with col1:
-        st.image("./assets/Idace.png", width=150)
+    # with col1:
+    #     st.image("./assets/Idace.png", width=150)
         
-    with col2:
-        st.image("./assets/CC_Terra.png", width=150)
+    # with col2:
+    #     st.image("./assets/CC_Terra.png", width=150)
 
-    with col3:
-        st.image("./assets/funcap.png", width=250)
+    # with col3:
+    #     st.image("./assets/funcap.png", width=250)
         
-    with col4:
-        st.image("./assets/ufc_logo.png", width=150)
+    # with col4:
+    #     st.image("./assets/ufc_logo.png", width=150)
 
 
 
